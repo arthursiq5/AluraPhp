3d5d250456a892dd9741bb4be6a24b8988f2ec9e
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-17 15:31:49 -0300

    inicio  de comandos SQL mais elaborados; introducao a pequenas operacoes aritmeticas com os valores de cada campo da tabela 'compras'

--- postgresql_1/comandos_sql_basico.let.sql
+++ postgresql_1/comandos_sql_basico.let.sql
@@ -31,3 +31,11 @@ SELECT * FROM compras WHERE valor <= 100; -- mostra todos os campos de todos os 
 SELECT * FROM compras WHERE data='06-22-2010'; -- mostra o item comprado dia 22 de junto de 2010 (formato de data americano)
 
 SELECT * FROM compras WHERE observacao='COMPRAS DE JANEIRO'; -- mostra o(s) item(ns) com a observacao igual a 'COMPRAS DE JANEIRO' (case sensitive)
+
+SELECT * FROM compras WHERE observacao like '%COMPRAS%'; -- seleciona todos os itens que possuem em algum lugar da descricao a palavra compras (o simbolo de porcentagem pode significar tanto 'nada' quanto qualquer quantidade de caracteres de qualquer valor)
+
+SELECT * FROM compras WHERE valor > 1000 AND data != '06-22-2010'; -- mostra todas as compras com valor acima de 1000 reais e que nao foram comprados em 22 de junho de 2010
+
+SELECT valor*0.2 AS imposto FROM compras; -- seleciona o campo 'valor' e retorna cada item multiplicado por 0.2 (20% de cada valor) em uma coluna com o nome 'imposto'
+
+SELECT valor, valor*0.2 from compras; -- retorna uma coluna com cada valor, e outra com o imposto correspondente
