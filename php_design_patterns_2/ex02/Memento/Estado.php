65a2b319f3c75af212dbc5b963008cf9eb4d6b10
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-20 16:03:03 -0300

    adicionada 'hora em que o Estado foi gerado' na classe Estado

--- php_design_patterns_2/ex02/Memento/Estado.php
+++ php_design_patterns_2/ex02/Memento/Estado.php
@@ -2,9 +2,11 @@
   namespace Memento;
   class Estado{
     private $contrato;
+    private $horaGerada;
 
     public function __construct(Contrato $contrato){
       $this->contrato = $contrato; // encapsula o contrato selecionado
+      $this->horaGerada = date("h:i:s");
     }
 
     public function getContrato(){
--- php_design_patterns_2/ex02/index.php
+++ php_design_patterns_2/ex02/index.php
@@ -1,16 +1,32 @@
 <?php
+  date_default_timezone_set('Brazil/East');
   require_once 'autoload.php';
   use Memento\Contrato;
+  use Memento\Historico;
 
-  $contrato = new Contrato("Caelum", date("Y-m-d"));
+  $historico = new Historico();
+  $contrato = new Contrato("Caelum", date("Y-m-d -> h:i:s"));
 
+  $historico->addEstado($contrato->salvaEstado());
   echo '<pre>';
   var_dump($contrato);
   echo '</pre>';
 
   $contrato->avanca();
 
+  $historico->addEstado($contrato->salvaEstado());
   echo '<pre>';
   var_dump($contrato);
+  echo '</pre>';
+
+  $contrato->avanca();
+
+  $historico->addEstado($contrato->salvaEstado());
+  echo '<pre>';
+  var_dump($contrato);
+  echo '</pre>';
+
+  echo '<pre>';
+  var_dump($historico->getEstado(0));
   echo '</pre>';
  ?>
