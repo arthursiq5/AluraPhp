6aa21af09f3d308151dbfd1ba6d7211d5b19de2b
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-20 17:17:53 -0300

    executados alguns testes de erro com o programa

--- php_design_patterns_2/ex03/index.php
+++ php_design_patterns_2/ex03/index.php
@@ -3,9 +3,12 @@
   require_once 'autoload.php';
   use Calculadora\Numero;
   use Calculadora\Soma;
+  use Calculadora\Subtracao;
+  use Calculadora\Multiplicacao;
+  use Calculadora\Divisao;
 
   $esquerdo = new Soma(new Numero(1), new Numero(3));
-  $direito = new Soma(new Numero(2), new Numero(2));
+  $direito = new Subtracao(new Multiplicacao(new Numero(3), new Numero(5)), new Numero(2));
 
   $soma = new Soma($esquerdo, $direito);
 
