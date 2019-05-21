60fcf8835e406a04b2546070472322a2d850b902
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-21 14:43:56 -0300

    implementado uma classe 'adapter' para as funcoes 'date'

---
+++ php_design_patterns_2/ex03/Data/Data.php
@@ -0,0 +1,9 @@
+<?php
+  namespace Data;
+
+  interface Data{
+    public function getDia();
+
+    public function getMes();
+  }
+ ?>
--- php_design_patterns_2/ex03/index.php
+++ php_design_patterns_2/ex03/index.php
@@ -7,12 +7,16 @@
   use Calculadora\Multiplicacao;
   use Calculadora\Divisao;
   use Calculadora\Impressora;
+  use Data\Relogio;
 
   $esquerdo = new Soma(new Numero(1), new Numero(3));
   $direito = new Subtracao(new Multiplicacao(new Numero(3), new Numero(5)), new Numero(2));
 
   $soma = new Soma($esquerdo, $direito);
+  $data = new Relogio();
+  echo $data->getDia() . '/' . $data->getMes();
+  echo '<br/>';
 
   $impressora = new Impressora();
-  echo '<br/>' . $soma->aceita($impressora)->avalia(); // imprimindo o calculo e seu resultado com funcoes encadeadas
+  echo ' = ' . $soma->aceita($impressora)->avalia(); // imprimindo o calculo e seu resultado com funcoes encadeadas
  ?>
