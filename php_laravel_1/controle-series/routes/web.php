b86f53f11acc6ab9b654384999f181cbf68816e5
Arthur Siqueira <arthur.siq5@gmail.com>
2019-05-22 15:41:20 -0300

    projeto encerrado

--- php_laravel_1/controle-series/routes/web.php
+++ php_laravel_1/controle-series/routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', 'SeriesController@listarSeries');
+//Route::get('/series', 'SeriesController@listarSeries');
 ?>
